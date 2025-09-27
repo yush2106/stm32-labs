63be4922988475efc52d71189d2d489b7aceb82f
yush2106 <gavinyu42@hotmail.com>
2025-09-27 23:18:09 +0800

    09-led-dice-hex-register

--- 09-led-dice-hex-register/Core/Src/main.c
+++ 09-led-dice-hex-register/Core/Src/main.c
@@ -18,7 +18,8 @@
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
-
+#include "stdlib.h"
+#include "time.h"
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
 
@@ -109,12 +110,12 @@ int main(void)
   while (1)
   {
     /* USER CODE END WHILE */
-	  //wait for button pressed
-	  while(HAL_GPIO_ReadPin(GPIOC, button) == 0) {
-		int dice_number = rand()%6;    //generate random dice number
-		GPIOC -> ODR = LED_Array[dice_number];    //write to output data register
-		HAL_Delay(5000);    //delay
-		Reset_All_Leds();    //Reset all Leds
+	//wait for button pressed
+	while(HAL_GPIO_ReadPin(GPIOC, button) == 0) {
+      int dice_number = rand()%6;    //generate random dice number
+      GPIOC -> ODR = LED_Array[dice_number];    //write to output data register
+      HAL_Delay(5000);    //delay
+      Reset_All_Leds();    //Reset all Leds
 	}
     /* USER CODE BEGIN 3 */
   }
